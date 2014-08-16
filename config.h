44d50a9da4c2358a11f296920e08d8331b4b49be
Lukasz Witkowski <witul@linux.pl>
2014-08-16 14:09:33 +0200

    poprawka dla passmenu

--- config.h
+++ config.h
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *passmenucmd[] = { "passmenu", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *passmenucmd[] = { "/home/witul/bin/passmenu", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 
 static const char *ss[]={"/home/witul/scripts/screenshot",NULL};
